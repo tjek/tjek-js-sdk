4fed06416f1f3a2f69da9932181682af4e1c78dd
klarstrup <jonas@klarstrup.dk>
2017-10-24 11:02:18 +0200

    Redo couple tests in coffeescript

--- __tests__/util_isbrowser_node.js
+++
@@ -1,9 +0,0 @@
-/**
- * @jest-environment node
- */
-SGN = require("../lib/coffeescript/node");
-
-test("isBrowser works right", () => {
-  expect(SGN.util.isBrowser()).toBeFalsy();
-  expect(SGN.util.isNode()).toBeTruthy();
-});
