b36e07f49fcc77d3e39a84cdefa7409de560059f
Morten Bo Rønsholdt <mr@shopgun.com>
2018-01-30 15:32:20 +0100

    Updated Incito browser lib

--- publish-pages.js
+++ publish-pages.js
@@ -1,12 +1,12 @@
 var ghpages = require('gh-pages');
 var path = require('path');
  
-ghpages.publish('dist', function (err) {
-    console.log(err);
-});
-
-// ghpages.publish('dist', {
-//     dest: 'incito'
-// }, function (err) {
+// ghpages.publish('dist', function (err) {
 //     console.log(err);
 // });
+
+ghpages.publish('dist', {
+    dest: 'incito'
+}, function (err) {
+    console.log(err);
+});
