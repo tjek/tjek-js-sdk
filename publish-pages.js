3dfc36ccccfa1c8a23682d6b0e3972f881d06eca
Morten Bo Rønsholdt <mr@shopgun.com>
2018-01-31 20:14:26 +0100

    Update deps

--- publish-pages.js
+++ publish-pages.js
@@ -1,9 +1,9 @@
 var ghpages = require('gh-pages');
 var path = require('path');
  
-// ghpages.publish('dist', function (err) {
-//     console.log(err);
-// });
+ghpages.publish('dist', function (err) {
+    console.log(err);
+});
 
 ghpages.publish('dist', {
     dest: 'incito'
