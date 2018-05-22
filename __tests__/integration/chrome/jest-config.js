c670c8dd0582ab672067322f69ab7a3ec47148f3
Morten Bo Rønsholdt <mr@shopgun.com>
2018-05-22 10:29:04 +0200

    Make tests pass

--- __tests__/integration/chrome/jest-config.js
+++ __tests__/integration/chrome/jest-config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  "preset": "jest-puppeteer",
-  rootDir: "../../../",
-  moduleFileExtensions: ["coffee", "js"],
-  transform: {
-    "^.+\\.(coffee)$": "<rootDir>/jest-preprocessor.js"
-  },
-  testMatch: ["**/__tests__/integration/chrome/**/*.(coffee|js)"],
-  testPathIgnorePatterns: ["jest-config.js","webpack-compiler.js"]
+    preset: "jest-puppeteer",
+    rootDir: "../../../",
+    moduleFileExtensions: ["coffee", "js"],
+    transform: {
+        "^.+\\.(coffee)$": "<rootDir>/jest-preprocessor.js"
+    },
+    testMatch: ["**/__tests__/integration/chrome/**/*.(coffee|js)"],
+    testPathIgnorePatterns: ["jest-config.js","webpack-compiler.js"]
 };
