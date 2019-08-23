ceef8469b7ff12dc06013328ad2e160250794738
Morten Bo Rønsholdt <mr@shopgun.com>
2019-08-23 12:31:29 +0200

    Remove Apple

--- dist/integrations/elgiganten/script-v2.6.js
+++ dist/integrations/elgiganten/script-v2.6.js
@@ -522,31 +522,6 @@
     }
     
     if (els.incito.categorySwitcher) {
-        var date = new Date();
-        var month = date.getMonth() + 1;
-        var day = date.getDate();
-        var year = date.getFullYear();
-        var showApple = false;
-    
-        if (year === 2019) {
-            if (month === 7 && day >= 26) {
-                showApple = true;
-            } else if (month === 8 && day <= 25) {
-                showApple = true;
-            }
-        }
-    
-        if (showApple) {
-            var appleOptionEl = document.createElement('option');
-    
-            appleOptionEl.value = 'apple';
-            appleOptionEl.textContent = 'Apple';
-
-            var secondEl = els.incito.categorySwitcher.childNodes[1];
-
-            secondEl.parentNode.insertBefore(appleOptionEl, secondEl.nextSibling);
-        }
-    
         els.incito.categorySwitcher.addEventListener('change', function (e) {
             var category = e.target.value;
     
