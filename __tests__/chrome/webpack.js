faacd2953f0e15462a233b0e31088def69b20a53
klarstrup <io@klarstrup.dk>
2020-12-30 13:50:38 +0100

    Expand & simplify webpack tests

--- __tests__/chrome/webpack.js
+++ __tests__/chrome/webpack.js
@@ -1,39 +1,31 @@
 const webpackCompiler = require('../../__tests_utils__/webpack-compiler');
+const each = require('jest-each').default;
 
 jest.setTimeout(30000); // this could take a while
 
-describe('Chrome + Webpack', () => {
-    it('SGN: Webpack compiles it in development mode and chrome runs it', async () => {
-        const inputCode = "var SGN = require('./'); console.log(SGN);";
-        const bundleCode = await webpackCompiler(inputCode, {
-            mode: 'development'
-        });
-        return await page.evaluate(bundleCode);
-    });
+function buildMatrix(options, index = 0, results = [], current = []) {
+    var vals = options[index];
 
-    it('SGN: Webpack compiles it in production mode and chrome runs it', async () => {
-        const inputCode = "var SGN = require('./'); console.log(SGN);";
-        const bundleCode = await webpackCompiler(inputCode, {
-            mode: 'production'
-        });
-        return await page.evaluate(bundleCode);
-    });
+    for (var i = 0; i < vals.length; i++) {
+        current[index] = vals[i];
 
-    it('EventsKit: Webpack compiles it in development mode and chrome runs it', async () => {
-        const inputCode =
-            "const EventsKit = require('./kits/events'); console.log(EventsKit)";
-        const bundleCode = await webpackCompiler(inputCode, {
-            mode: 'development'
-        });
-        return await page.evaluate(bundleCode);
-    });
+        if (index + 1 < options.length)
+            buildMatrix(options, index + 1, results, current);
+        else results.push([...current]);
+    }
 
-    it('EventsKit: Webpack compiles it in production mode and chrome runs it', async () => {
-        const inputCode =
-            "const EventsKit = require('./kits/events'); console.log(EventsKit)";
-        const bundleCode = await webpackCompiler(inputCode, {
-            mode: 'production'
-        });
-        return await page.evaluate(bundleCode);
-    });
+    return results;
+}
+each(
+    buildMatrix([
+        ['./', './kits/events'],
+        ['development', 'production'],
+        ['require', 'import']
+    ])
+).test('Webpack + Chrome: %s %s %s', async (path, mode, pkg) => {
+    const code =
+        pkg === 'import'
+            ? `import A from '${path}'; console.log(A)`
+            : `const A = require('${path}'); console.log(A)`;
+    await page.evaluate(await webpackCompiler(code, {mode}));
 });
