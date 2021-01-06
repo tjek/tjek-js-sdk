f507132c845a02cda2b6cf92c2c7bedd2bedf811
klarstrup <io@klarstrup.dk>
2021-01-06 12:39:20 +0100

    Simplify & speed up test running

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    testPathIgnorePatterns: ['node_modules'],
+    projects: [
+        {
+            displayName: 'Node',
+            testEnvironment: 'node',
+            testRegex: '((\\.|/)(test|test-node))\\.js$'
+        },
+        {
+            displayName: 'JSDom',
+            testEnvironment: 'jsdom',
+            testRegex: '((\\.|/)(test|test-jsdom))\\.js$'
+        },
+        {
+            displayName: 'Chrome',
+            preset: 'jest-puppeteer-preset',
+            testRegex: '((\\.|/)(test-chrome))\\.js$'
+        }
+    ]
+};
