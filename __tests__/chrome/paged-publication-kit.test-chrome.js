0dc020e067a9de6158fea854b7a086b2046a2a40
klarstrup <jonas@klarstrup.dk>
2021-01-06 17:36:07 +0100

    Simplify PPKit Chrome test

--- __tests__/chrome/paged-publication-kit.test-chrome.js
+++ __tests__/chrome/paged-publication-kit.test-chrome.js
@@ -9,25 +9,22 @@ const PPPath = `file:${path.join(
     'dist',
     'paged-publication.html'
 )}`;
-const getPPPage = async () => {
-    const page = await browser.newPage();
-    await page.goto(PPPath);
-    return page;
-};
 
 describe('Chrome: Paged Publication', () => {
+    let page;
+    beforeEach(async () => {
+        page = await browser.newPage();
+        await page.goto(PPPath);
+    });
     it('Should display "Make your PDF alive with ShopGun." text on page', async () => {
-        const page = await getPPPage();
         await expect(page).toMatch('Make your PDF alive with ShopGun.');
     });
 
     it('Should load with the intro active', async () => {
-        const page = await getPPPage();
         await expect(page).toMatchElement('#intro[data-active="true"]');
     });
 
     it('Should go to next page when clicking the next page thingie', async () => {
-        const page = await getPPPage();
         await expect(page).toMatchElement('#intro[data-active="true"]');
         await expect(page).toClick('.sgn-pp__control[data-direction="next"]');
         await expect(page).not.toMatchElement('#intro[data-active="true"]');
