cbc29b7b0ae600d5602e3e91f37b580adf740c7f
klarstrup <io@klarstrup.dk>
2021-01-08 19:28:44 +0100

    Colocate styles with consumers

--- jest.config.js
+++ jest.config.js
@@ -5,17 +5,26 @@ module.exports = {
         {
             displayName: 'Node',
             testEnvironment: 'node',
-            testRegex: '((\\.|/)(test|test-node))\\.js$'
+            testRegex: '((\\.|/)(test|test-node))\\.js$',
+            moduleNameMapper: {
+                '\\.(css|styl)$': '<rootDir>/__tests_utils__/style-mock.js'
+            }
         },
         {
             displayName: 'JSDom',
             testEnvironment: 'jsdom',
-            testRegex: '((\\.|/)(test|test-jsdom))\\.js$'
+            testRegex: '((\\.|/)(test|test-jsdom))\\.js$',
+            moduleNameMapper: {
+                '\\.(css|styl)$': '<rootDir>/__tests_utils__/style-mock.js'
+            }
         },
         {
             displayName: 'Chrome',
             preset: 'jest-puppeteer-preset',
-            testRegex: '((\\.|/)(test-chrome))\\.js$'
+            testRegex: '((\\.|/)(test-chrome))\\.js$',
+            moduleNameMapper: {
+                '\\.(css|styl)$': '<rootDir>/__tests_utils__/style-mock.js'
+            }
         }
     ]
 };
