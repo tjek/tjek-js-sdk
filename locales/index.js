90d409581d5ede4a3639e98e0b0f8c063c76225c
Jevy Roque H. Laraño <jevyroque@gmail.com>
2022-01-31 17:53:49 +0800

    [Template] Add sv_SE and nb_NO locales

--- locales/index.js
+++ locales/index.js
@@ -1,2 +1,4 @@
 export {default as da_dk} from './da_DK';
 export {default as en_us} from './en_US';
+export {default as sv_se} from './sv_SE';
+export {default as nb_no} from './nb_NO';
---
+++ locales/sv_SE.js
@@ -0,0 +1,16 @@
+export default {
+    locale_code: 'sv_SE',
+    paged_publication_viewer_shopping_list_label: 'Inköpslista',
+    paged_publication_viewer_shopping_list_clear_button: 'Tydlig lista',
+    paged_publication_viewer_delete_crossed_out_button:
+        'Ta bort överstrukna objekt',
+    paged_publication_viewer_print_button: 'Skriva ut',
+    paged_publication_viewer_download_button: 'Ladda ner',
+    paged_publication_viewer_until_label: 'Fram tills',
+    paged_publication_viewer_pages_button: 'Sidor',
+    paged_publication_viewer_offers_button: 'Erbjudanden',
+    paged_publication_viewer_search_text: 'Sök',
+    paged_publication_viewer_currency: 'SEK',
+    paged_publication_viewer_hotspot_picker_header:
+        'Vilket erbjudande menade du?'
+};
