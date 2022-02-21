94215ab10d29e05d4a38579adabe5c607c62e973
Jevy Roque H. Laraño <jevyroque@gmail.com>
2022-02-21 19:35:45 +0800

    [Template][Refactor] Initial consolidation of PDF and Incito viewer

--- locales/en_US.js
+++ locales/en_US.js
@@ -1,16 +1,15 @@
 export default {
     locale_code: 'en_US',
-    paged_publication_viewer_shopping_list_label: 'Shopping List',
-    paged_publication_viewer_shopping_list_clear_button: 'Clear list',
-    paged_publication_viewer_delete_crossed_out_button:
-        'Delete crossed out items',
-    paged_publication_viewer_print_button: 'Print',
-    paged_publication_viewer_download_button: 'Download',
-    paged_publication_viewer_until_label: 'Through',
-    paged_publication_viewer_pages_button: 'Pages',
-    paged_publication_viewer_offers_button: 'Offers',
-    paged_publication_viewer_search_text: 'Search',
-    paged_publication_viewer_currency: 'USD',
-    paged_publication_viewer_hotspot_picker_header: 'Which offer did you mean?',
-    incito_publication_viewer_overview_button: 'Overview'
+    publication_viewer_shopping_list_label: 'Shopping List',
+    publication_viewer_shopping_list_clear_button: 'Clear list',
+    publication_viewer_delete_crossed_out_button: 'Delete crossed out items',
+    publication_viewer_print_button: 'Print',
+    publication_viewer_download_button: 'Download',
+    publication_viewer_until_label: 'Through',
+    publication_viewer_pages_button: 'Pages',
+    publication_viewer_offers_button: 'Offers',
+    publication_viewer_search_text: 'Search',
+    publication_viewer_currency: 'USD',
+    publication_viewer_hotspot_picker_header: 'Which offer did you mean?',
+    publication_viewer_overview_button: 'Overview'
 };
--- locales/nb_NO.js
+++ locales/nb_NO.js
@@ -1,16 +1,16 @@
 export default {
     locale_code: 'nb_NO',
-    paged_publication_viewer_shopping_list_label: 'Handleliste',
-    paged_publication_viewer_shopping_list_clear_button: 'Klar liste',
-    paged_publication_viewer_delete_crossed_out_button:
+    publication_viewer_shopping_list_label: 'Handleliste',
+    publication_viewer_shopping_list_clear_button: 'Klar liste',
+    publication_viewer_delete_crossed_out_button:
         'Slett overstrekede elementer',
-    paged_publication_viewer_print_button: 'Skrive ut',
-    paged_publication_viewer_download_button: 'nedlasting',
-    paged_publication_viewer_until_label: 'Igjennom',
-    paged_publication_viewer_pages_button: 'Sider',
-    paged_publication_viewer_offers_button: 'Tilbud',
-    paged_publication_viewer_search_text: 'Søk',
-    paged_publication_viewer_currency: 'NOK',
-    paged_publication_viewer_hotspot_picker_header: 'Hvilket tilbud mente du?',
-    incito_publication_viewer_overview_button: 'oversikt'
+    publication_viewer_print_button: 'Skrive ut',
+    publication_viewer_download_button: 'nedlasting',
+    publication_viewer_until_label: 'Igjennom',
+    publication_viewer_pages_button: 'Sider',
+    publication_viewer_offers_button: 'Tilbud',
+    publication_viewer_search_text: 'Søk',
+    publication_viewer_currency: 'NOK',
+    publication_viewer_hotspot_picker_header: 'Hvilket tilbud mente du?',
+    publication_viewer_overview_button: 'oversikt'
 };
