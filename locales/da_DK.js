54c499cac1f01f9cd6679a9d86ce16bb9c59f1ec
Jevy Larano <32891190+jrlarano@users.noreply.github.com>
2022-03-09 19:36:11 +0800

    [Template] Incito viewer (#106)
    * [Template] Initial commit for Incito viewer
    
    * [Template] Add scroll to offer and section
    
    * [Template] Add types to request and add preferred type
    
    * [Template] Add Section list in incito
    
    * Fix shopping list format for sharing
    
    * [Incito template] Fix shopping list without price
    
    * [Template Incito] Filter offers for incito
    
    * [Tempate incito] Add loader and scroll progress level
    
    * [template incito] Reuse shopping list from paged publication viewer
    
    * [Template incito] Add progress percentage
    
    * [Template Incito] Remove console logs
    
    * [Template] Fix Shopping list print layout
    
    * [Template][Refactor] Initial consolidation of PDF and Incito viewer
    
    * [Template] Add optional attributes to pub list sample
    
    * [Template] Use single container for PDF and Incito viewer
    
    * [Template] Unify Header component
    
    * [Template] Move header to common
    
    * [Template] Optimize header for incito and paged
    
    * [Template] Add tjek link to readme
    
    * [Template] change header color based on brightness
    
    * [Template] Consolidate menu-popup and offer list for incito and paged
    
    * Fix lint error
    
    * [Template Incito] Support custom template for section list
    
    * [Template] Optimize header styling
    
    * [Template] Fix header z-index
    
    * [Template] Fix progress label
    
    * [Template Incito] Fix webshop link on offer click
    
    * [Template Incito] dispatch event on offer click
    
    * [Template] Add offset to section scroll
    
    * [Template Incito] Fetch offer on cell click
    
    * [Template Incito] Add offer click tracking
    
    * [Incito template] Move tracker to fetchOffer function
    
    * [Template Incito] Refactor fetch offer param names
    
    * Remove fetchOffer from incito Bootstrapper & don't make integrators make redundant fetchOffer call
    
    * Fix template examples relative paths
    
    * sort imports
    
    Co-authored-by: klarstrup <jonas@klarstrup.dk>

--- locales/da_DK.js
+++ locales/da_DK.js
@@ -1,15 +1,15 @@
 export default {
     locale_code: 'da_DK',
-    paged_publication_viewer_shopping_list_label: 'Indkøbsliste',
-    paged_publication_viewer_shopping_list_clear_button: 'Ryd indkøbsliste',
-    paged_publication_viewer_delete_crossed_out_button:
-        'Slet overstregede varer',
-    paged_publication_viewer_print_button: 'Print',
-    paged_publication_viewer_download_button: 'Hent',
-    paged_publication_viewer_until_label: 'Til og med',
-    paged_publication_viewer_pages_button: 'Sider',
-    paged_publication_viewer_offers_button: 'Tilbud',
-    paged_publication_viewer_search_text: 'Søg',
-    paged_publication_viewer_currency: 'DKK',
-    paged_publication_viewer_hotspot_picker_header: 'Hvilket tilbud mente du?'
+    publication_viewer_shopping_list_label: 'Indkøbsliste',
+    publication_viewer_shopping_list_clear_button: 'Ryd indkøbsliste',
+    publication_viewer_delete_crossed_out_button: 'Slet overstregede varer',
+    publication_viewer_print_button: 'Print',
+    publication_viewer_download_button: 'Hent',
+    publication_viewer_until_label: 'Til og med',
+    publication_viewer_pages_button: 'Sider',
+    publication_viewer_offers_button: 'Tilbud',
+    publication_viewer_search_text: 'Søg',
+    publication_viewer_currency: 'DKK',
+    publication_viewer_hotspot_picker_header: 'Hvilket tilbud mente du?',
+    publication_viewer_overview_button: 'Oversigt'
 };
--- locales/sv_SE.js
+++ locales/sv_SE.js
@@ -1,16 +1,15 @@
 export default {
     locale_code: 'sv_SE',
-    paged_publication_viewer_shopping_list_label: 'Inköpslista',
-    paged_publication_viewer_shopping_list_clear_button: 'Tydlig lista',
-    paged_publication_viewer_delete_crossed_out_button:
-        'Ta bort överstrukna objekt',
-    paged_publication_viewer_print_button: 'Skriva ut',
-    paged_publication_viewer_download_button: 'Ladda ner',
-    paged_publication_viewer_until_label: 'Till slut',
-    paged_publication_viewer_pages_button: 'Sidor',
-    paged_publication_viewer_offers_button: 'Erbjudanden',
-    paged_publication_viewer_search_text: 'Sök',
-    paged_publication_viewer_currency: 'SEK',
-    paged_publication_viewer_hotspot_picker_header:
-        'Vilket erbjudande menade du?'
+    publication_viewer_shopping_list_label: 'Inköpslista',
+    publication_viewer_shopping_list_clear_button: 'Tydlig lista',
+    publication_viewer_delete_crossed_out_button: 'Ta bort överstrukna objekt',
+    publication_viewer_print_button: 'Skriva ut',
+    publication_viewer_download_button: 'Ladda ner',
+    publication_viewer_until_label: 'Till slut',
+    publication_viewer_pages_button: 'Sidor',
+    publication_viewer_offers_button: 'Erbjudanden',
+    publication_viewer_search_text: 'Sök',
+    publication_viewer_currency: 'SEK',
+    publication_viewer_hotspot_picker_header: 'Vilket erbjudande menade du?',
+    publication_viewer_overview_button: 'Översikt'
 };
