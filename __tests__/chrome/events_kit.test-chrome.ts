d8f6c8ca0ca6bf1e231f8f8fe47682b65b01e77f
io <io@klarstrup.dk>
2023-04-03 12:10:27 +0200

    feat(EventTracker): Use sendBeacon instead of Fetch with dispatch on unload rather than on next load. (#172)
    * feat(EventTracker): Replace persisted pool with sendBeacon
    
    * refactor(EventTracker): Only use sendBeacon, ditch validation and retrying
    
    * chore(browserslist): require supports beacon
    
    * feat(Tracker): Add best-effort sendBeacon ponyfill
    
    This regains us some coverage without very much overhead.
    
    Ita also allows running the EventsKit tests in our sendBeacon-less node(fetch)/jsdom(XMLHttpRequest) test environments.
    
    * fix(Tracker): Don't check visibilityState on beforeUnload
    
    * tests(EventsKit): Test onBeforeUnload dispatch
    
    Sharpen the delayed dispatch tests
    
    * feat(Tracker): Defer dispatch with debounce
    
    Throttle results in a single event instantly dispatching without batching
    
    Augmented throttle types too
    
    * test(events): Be more patient with beforeUnload dispatch
    
    * test(events): Be more patient with beforeUnload dispatch
    
    * test(events): Be more patient with beforeUnload dispatch
    
    * test(events): Be more patient with beforeUnload dispatch
    
    * test(events): goto blank instead of close page

--- __tests__/chrome/events_kit.test-chrome.ts
+++ __tests__/chrome/events_kit.test-chrome.ts
@@ -2,7 +2,7 @@
 /// <reference types="expect-playwright" />
 
 describe('SGN.EventsKit', () => {
-    test('Dispatched Paged Publication Opened events become /sync requests', async () => {
+    test('Dispatched Paged Publication Opened events become /sync requests eventually', async () => {
         const eventsSent: any[] = [];
         const page = await (await browser.newContext()).newPage();
         page.on('request', (interceptedRequest) => {
@@ -17,12 +17,13 @@ describe('SGN.EventsKit', () => {
             `file:${__dirname}/../../examples/paged-publication.html`
         );
 
-        await page.waitForTimeout(9000);
+        await page.waitForLoadState('load');
+        await page.waitForTimeout(8000);
 
         expect(eventsSent.length).toBeGreaterThan(0);
         expect(eventsSent.filter(({_e}) => _e === 1).length).toBe(1);
     });
-    test('Dispatched Incito Publication Opened events become /sync requests', async () => {
+    test('Dispatched Incito Publication Opened events become /sync requests eventually', async () => {
         const response = await fetch(
             'https://squid-api.tjek.com/v2/catalogs?limit=1&types=incito'
         );
@@ -31,10 +32,7 @@ describe('SGN.EventsKit', () => {
         const eventsSent: any[] = [];
         const page = await (await browser.newContext()).newPage();
         page.on('request', (interceptedRequest) => {
-            if (
-                interceptedRequest.url().endsWith('/sync') &&
-                interceptedRequest.method() === 'POST'
-            ) {
+            if (interceptedRequest.url().endsWith('/sync')) {
                 eventsSent.push(...interceptedRequest.postDataJSON().events);
             }
         });
@@ -42,7 +40,51 @@ describe('SGN.EventsKit', () => {
             `file:${__dirname}/../../examples/incito-publication.html?id=${id}`
         );
 
-        await page.waitForTimeout(9000);
+        await page.waitForLoadState('load');
+        await page.waitForTimeout(8000);
+
+        expect(eventsSent.length).toBeGreaterThan(0);
+        expect(eventsSent.filter(({_e}) => _e === 11).length).toBe(1);
+    });
+    test('Dispatched Paged Publication Opened events become /sync requests on page close', async () => {
+        const eventsSent: any[] = [];
+        const page = await (await browser.newContext()).newPage();
+        page.on('request', (interceptedRequest) => {
+            if (interceptedRequest.url().endsWith('/sync')) {
+                eventsSent.push(...interceptedRequest.postDataJSON().events);
+            }
+        });
+        await page.goto(
+            `file:${__dirname}/../../examples/paged-publication.html`
+        );
+        await page.waitForLoadState('load');
+        await page.waitForTimeout(1000);
+        await page.goto('about:blank');
+        await new Promise((y) => setTimeout(y, 1000));
+
+        expect(eventsSent.length).toBeGreaterThan(0);
+        expect(eventsSent.filter(({_e}) => _e === 1).length).toBe(1);
+    });
+    test('Dispatched Incito Publication Opened events become /sync requests on page close', async () => {
+        const response = await fetch(
+            'https://squid-api.tjek.com/v2/catalogs?limit=1&types=incito'
+        );
+        const id = (await response.json())?.[0]?.id;
+
+        const eventsSent: any[] = [];
+        const page = await (await browser.newContext()).newPage();
+        page.on('request', (interceptedRequest) => {
+            if (interceptedRequest.url().endsWith('/sync')) {
+                eventsSent.push(...interceptedRequest.postDataJSON().events);
+            }
+        });
+        await page.goto(
+            `file:${__dirname}/../../examples/incito-publication.html?id=${id}`
+        );
+        await page.waitForLoadState('load');
+        await page.waitForTimeout(1000);
+        await page.goto('about:blank');
+        await new Promise((y) => setTimeout(y, 1000));
 
         expect(eventsSent.length).toBeGreaterThan(0);
         expect(eventsSent.filter(({_e}) => _e === 11).length).toBe(1);
