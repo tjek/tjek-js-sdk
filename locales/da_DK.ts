1c2e90ba4f4dc3e5255398a36adedb4ada16ee9e
Jevy Larano <32891190+jrlarano@users.noreply.github.com>
2023-08-14 15:37:06 +0800

    remove avis/tidning

--- locales/da_DK.ts
+++ locales/da_DK.ts
@@ -20,5 +20,5 @@ export default {
     publication_viewer_close_label: 'Tilbage',
     publication_viewer_add_to_shopping_list: 'Tilføj til indkøbsliste',
     publication_viewer_visit_webshop_link: 'Besøg webshoplink',
-    publication_viewer_upcoming: 'Kommende avis'
+    publication_viewer_upcoming: 'Kommende'
 };
