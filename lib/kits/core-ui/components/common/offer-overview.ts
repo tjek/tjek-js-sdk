3c1e384e4a3e68effa0ada670b4c2e54c38b82e9
Tereza Lansing <tereza@lansing.dk>
2024-06-28 14:08:51 +0200

    move mobile price above quantity

--- lib/kits/core-ui/components/common/offer-overview.ts
+++ lib/kits/core-ui/components/common/offer-overview.ts
@@ -121,18 +121,20 @@ const defaultTemplateV2 = `\
                                     </div>
                                     <div class="sgn-product-description"><span>{{description}}</span></div>
                                 </div>
-                                <div id="sgn-offer-product-quantity-{{id}}" class="sgn-offer-product-quantity">
-                                    <div class="sgn-offer-product-quantity-content">
-                                        <button id="sgn-offer-product-quantity-minus-{{id}}" class="sgn-offer-product-quantity-minus">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="bi bi-minus-circle-fill" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z"/></svg>
-                                        </button>
-                                        <input type="text" id="sgn-offer-product-quantity-text-{{id}}" class="sgn-offer-product-quantity-text" value="{{quantity}}" disabled/>
-                                        <button id="sgn-offer-product-quantity-plus-{{id}}" class="sgn-offer-product-quantity-plus">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="bi bi-plus-circle-fill" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/></svg>
-                                        </button>
+                                <div class="sgn-product-price-container">
+                                    <div id="sgn-offer-product-quantity-{{id}}" class="sgn-offer-product-quantity">
+                                        <div class="sgn-offer-product-quantity-content">
+                                            <button id="sgn-offer-product-quantity-minus-{{id}}" class="sgn-offer-product-quantity-minus">
+                                                <svg xmlns="http://www.w3.org/2000/svg" class="bi bi-minus-circle-fill" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z"/></svg>
+                                            </button>
+                                            <input type="text" id="sgn-offer-product-quantity-text-{{id}}" class="sgn-offer-product-quantity-text" value="{{quantity}}" disabled/>
+                                            <button id="sgn-offer-product-quantity-plus-{{id}}" class="sgn-offer-product-quantity-plus">
+                                                <svg xmlns="http://www.w3.org/2000/svg" class="bi bi-plus-circle-fill" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/></svg>
+                                            </button>
+                                        </div>
                                     </div>
+                                    <div class="sgn-product-price"><span>{{formattedPrice}}</span></div>
                                 </div>
-                                <div class="sgn-product-price"><span>{{formattedPrice}}</span></div>
                             </div>
                             {{/products}}
                         </div>
@@ -208,7 +210,6 @@ const OfferOverview = ({
         dispatchOfferClickEvent(transformedOffer);
         addEventListeners();
     };
-
     const transformProducts = (offer, products) => {
         const {localeCode, currency} = translations;
         const storedPublicationOffers =
