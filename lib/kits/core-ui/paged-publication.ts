ff546eef9f1d430e30a34e197dfae27a0c1d9e56
Jevy Larano <32891190+jrlarano@users.noreply.github.com>
2025-03-13 12:54:16 +0800

    Refactor unsupported DOMSubtreeModified

--- lib/kits/core-ui/paged-publication.ts
+++ lib/kits/core-ui/paged-publication.ts
@@ -211,10 +211,6 @@ const PagedPublication = (
                 ?.classList.add('sgn-paged-publication--open');
         }
 
-        // const hotspots = await bootstrapper.fetchHotspots();
-        // console.log('hotspots:', hotspots);
-        // bootstrapper.applyHotspots(sgnViewer, hotspots, sgnPageDecorations);
-
         displayUrlParams();
         addFirstLastControlListener();
     };
@@ -312,20 +308,38 @@ const PagedPublication = (
                 '.sgn-pp__progress-label'
             );
 
-            progressLabel?.addEventListener('DOMSubtreeModified', (e) => {
-                const pageNum = e.target.innerHTML
-                    ?.split(' ')?.[0]
-                    ?.split('-')?.[0];
+            if (progressLabel) {
+                const observer = new MutationObserver((mutations) => {
+                    mutations.forEach((mutation) => {
+                        if (
+                            mutation.type === 'childList' ||
+                            mutation.type === 'characterData'
+                        ) {
+                            const pageNum = mutation.target.textContent
+                                ?.split(' ')?.[0]
+                                ?.split('-')?.[0];
 
-                if (scriptEls.displayUrlParams?.toLowerCase() === 'query') {
-                    pushQueryParam({
-                        [scriptEls.publicationIdParam]: options.id,
-                        [scriptEls.pageIdParam]: pageNum
+                            if (
+                                scriptEls.displayUrlParams?.toLowerCase() ===
+                                'query'
+                            ) {
+                                pushQueryParam({
+                                    [scriptEls.publicationIdParam]: options.id,
+                                    [scriptEls.pageIdParam]: pageNum
+                                });
+                            } else {
+                                location.hash = `${scriptEls.publicationHash}/${options.id}/${pageNum}`;
+                            }
+                        }
                     });
-                } else {
-                    location.hash = `${scriptEls.publicationHash}/${options.id}/${pageNum}`;
-                }
-            });
+                });
+
+                observer.observe(progressLabel, {
+                    childList: true,
+                    characterData: true,
+                    subtree: true
+                });
+            }
         }
     };
 
